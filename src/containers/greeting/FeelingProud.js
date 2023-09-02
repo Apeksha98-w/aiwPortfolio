282eff639f634a18cff3cbde9f427c095f536de3
Apeksha <apekshawarnakulasooriya98@gmail.com>
2023-09-02 23:15:21 +0530

    first commit

---
+++ src/containers/greeting/FeelingProud.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class FeelingProud extends Component {
+  render() {
+    const theme = this.props.theme;
+
+    // You need to return JSX code from the render method
+    return (
+      <div>
+        {/* JSX content goes here */}
+      </div>
+    );
+  }
+}
+
+export default FeelingProud;
+
